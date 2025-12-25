0256838ce115593356422dbf772a01960ed5feef
siddharth shukla <siddharthshukla.cs24@bmsce.ac.in>
2025-12-25 22:54:43 +0530

    Delete as on 17.12 directory

--- as on 17.12/frontend/static/js/tmdb_key.js
+++
@@ -1,9 +0,0 @@
-async function saveKey() {
-    const apiKey = document.getElementById("key").value.trim();
-    await fetch("/tmdb/key", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ apiKey })
-});
-    location.reload();
-}
